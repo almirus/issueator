47577f329893bfa5b2dcc9e0f42f6e3c835e976f
almirus <almir.abrarov@gmail.com>
2020-09-24 23:21:20 +0300

    [*] функция получения активного фрейма теперь возвращает Document

--- js/src/utils/getActiveFrame.js
+++ js/src/utils/getActiveFrame.js
@@ -1,16 +1,16 @@
 import {IFRAME_SELECTOR} from "./const";
 
-// ищем в DOM активный (открытый) iframe если не найдем возвращаем html.body
+// ищем в DOM активный (открытый) iframe если не найдем возвращаем основное окно
 // специфично для jepria gwt приложений
 export function getActiveFrame() {
-    let result = document.body;
+    let result = document;
     let url = document.URL;
     Array.from(document.getElementsByClassName(IFRAME_SELECTOR)).forEach(element => {
         if (element.nodeName === 'IFRAME') {
             if (element.parentElement.nodeName === 'DIV') {
                 if (element.parentElement.parentElement?.nodeName === 'DIV' && element.parentElement.parentElement.className === 'gwt-TabLayoutPanelContent') {
                     if (element.parentElement.parentElement?.parentElement?.nodeName === 'DIV' && element.parentElement.parentElement.parentElement.style.display !== 'none') {
-                        result = element.contentWindow.document.body;
+                        result = element.contentWindow.document;
                         url = element.contentWindow.location.href
                         console.log('active iFrame was found');
                     }
@@ -20,6 +20,6 @@ export function getActiveFrame() {
     })
     return {
         url: url,
-        body: result
+        document: result
     };
 }
